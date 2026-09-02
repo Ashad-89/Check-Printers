366fe7b5e0bf1a88331fa651ce7d599c3c3b6919
Ashad-89 <ash9932@hotmail.co.uk>
2026-09-02 15:32:33 +0100

    Update file paths to use relative references

--- Check-Printers.ps1
+++ Check-Printers.ps1
@@ -1,27 +1,24 @@
-﻿# Import the printer list from CSV
-$Printers = Import-Csv "C:\Users\Swytch Admin\Desktop\Home Printer Health Check Automation\printers.csv"
+$Printers = Import-Csv ".\printers.csv"
 
-# Loop through each printer and test connectivity
-$Results = foreach ($Printer in $Printers) {
-
+$Results = foreach ($Printer in $Printers)
+{
     $Online = Test-Connection `
         -ComputerName $Printer.IPAddress `
         -Count 2 `
         -Quiet
 
     [PSCustomObject]@{
-        PrinterName  = $Printer.PrinterName
-        IPAddress    = $Printer.IPAddress
-        Location     = $Printer.Location
-        Online       = $Online
-        DateChecked  = Get-Date
+        PrinterName = $Printer.PrinterName
+        IPAddress   = $Printer.IPAddress
+        Location    = $Printer.Location
+        Online      = $Online
+        DateChecked = Get-Date
     }
 }
 
-# Export results to CSV
-$Results | Export-Csv `
-    "C:\Users\Swytch Admin\Desktop\Home Printer Health Check Automation\PrinterStatus.csv" `
-    -NoTypeInformation
+$Results |
+Export-Csv ".\PrinterStatus.csv" `
+-NoTypeInformation
 
-# Display results in the console
 $Results
+``
