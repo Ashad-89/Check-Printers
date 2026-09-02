7c78a51bf101dbe3f49243d4eb4a36377dbcf057
Ashad <ash9932@hotmail.co.uk>
2026-09-02 15:03:25 +0100

    Added all files

---
+++ Check-Printers.ps1
@@ -0,0 +1,27 @@
+﻿# Import the printer list from CSV
+$Printers = Import-Csv "C:\Users\Swytch Admin\Desktop\Home Printer Health Check Automation\printers.csv"
+
+# Loop through each printer and test connectivity
+$Results = foreach ($Printer in $Printers) {
+
+    $Online = Test-Connection `
+        -ComputerName $Printer.IPAddress `
+        -Count 2 `
+        -Quiet
+
+    [PSCustomObject]@{
+        PrinterName  = $Printer.PrinterName
+        IPAddress    = $Printer.IPAddress
+        Location     = $Printer.Location
+        Online       = $Online
+        DateChecked  = Get-Date
+    }
+}
+
+# Export results to CSV
+$Results | Export-Csv `
+    "C:\Users\Swytch Admin\Desktop\Home Printer Health Check Automation\PrinterStatus.csv" `
+    -NoTypeInformation
+
+# Display results in the console
+$Results
